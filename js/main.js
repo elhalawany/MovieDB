5c6727b33dc67d412135e21d7bf69034ba1eb8ae
elhalawany <mohammed.aomwr@gmail.com>
2022-06-25 17:15:47 +0200

    add infinity Scroll

--- js/main.js
+++ js/main.js
@@ -1,48 +1,68 @@
+// *************************** بسم الله الرحمن الرحيم ************************** \\
+
 // Tooltip inintialization
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-// feth data from API
-
-
+// feth data from API Url
 const fetchData = async (url) => {
     const response = await fetch(url)
     const data = await response.json()
-
     return data
 }
 
-(async function (){
-    let data = await fetchData('https://api.themoviedb.org/3/movie/popular?api_key=eba8b9a7199efdcb0ca1f96879b83c44&page=2');
-    console.log(data.results);
-})();   
 
+// render movies to html 
+async function renderMovies(p) {
 
-
-(async function renderMovies() {
-
-    let data = await fetchData('https://api.themoviedb.org/3/movie/popular?api_key=eba8b9a7199efdcb0ca1f96879b83c44&page=1');
-    console.log(data.results);
+    let data = await fetchData(`https://api.themoviedb.org/3/movie/popular?api_key=eba8b9a7199efdcb0ca1f96879b83c44&page=${p}`);
+    //console.log(data.results);
     let movies = data.results;
-    let moviesData = document.querySelector('#movies-data');
+    let moviesDataContainer = document.querySelector('#movies-data');
 
 
     movies.forEach(movie => {
-        let movieDiv = `
-        <div class="col-md-6 col-lg-4 my-3">
+        let moviesDiv = document.createElement('div');
+        moviesDiv.classList.add('col-md-6', 'col-lg-4', 'my-3');
+
+        moviesDiv.innerHTML = `
             <div class="card shadow-lg rounded position-relative">  
                 <img class="img-fluid rounded" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" >
             </div>
-        </div>
         `;
-        moviesData.innerHTML += movieDiv;
+        moviesDataContainer.appendChild(moviesDiv);
         
     }
     );
-
-})();
-
-
-
+};
+// render on first load
+renderMovies(1);
 
 
+
+// load more movies on scroll (infinity scroll)
+let counterScroll = 2;
+document.addEventListener('scroll', function(e) {
+
+     const {
+            scrollTop,
+            scrollHeight,
+            clientHeight
+        } = document.documentElement;
+
+
+    if (scrollTop + clientHeight >= scrollHeight - 5 ) {
+        // console.log('scrolled to bottom');
+        counterScroll++;
+        renderMovies(counterScroll);
+    }
+    
+  });
+
+
+  // dark mode toggle 
+    const darkModeToggle = document.querySelector('#dark-mode-toggle');
+    function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
